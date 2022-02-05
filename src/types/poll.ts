a289795be6ca7cb0c95f07e873cc425d54eaa96a
gaepoij <henri.lahti9@gmail.com>
2022-02-05 03:26:55 +0200

    add PollResult interface

--- src/types/poll.ts
+++ src/types/poll.ts
@@ -4,8 +4,16 @@ export interface PollOption {
   type: string;
 }
 
+export interface PollOptionWithCount extends PollOption {
+  count: number;
+}
+
 export interface Poll {
   id: string;
   title: string;
   options: PollOption[];
+}
+
+export interface PollResult extends Poll {
+  options: PollOptionWithCount[];
 }
